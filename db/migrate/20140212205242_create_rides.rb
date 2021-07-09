8b933383caeac7629598dab1db9a2bfd2e6d32ea
yatsun <yatsunlam25@gmail.com>
2021-07-09 00:13:43 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.belongs_to :passenger_id
+      t.belongs_to :ride_id
       t.timestamps null: false
     end
   end
